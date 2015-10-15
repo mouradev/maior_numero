1e7bd1558e36b9c7c4d03ed3344f4b21e7727de3
Lucas Moura <lucasmoura765@gmail.com>
2015-10-15 13:32:40 -0300

    aumentado o número de exemplos

--- main.c
+++ main.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 
 void main() {
-    float notas[3], aux = 0;
+    float notas[10], aux = 0;
     int i, j;
 
-    for(i = 0; i < 3; i++) {
+    for(i = 0; i < 10; i++) {
         printf("digite a %da nota: ", i+1);
         scanf("%f", &notas[i]);
     }
 
-    for(j = 0; j < 3; j++) {
+    for(j = 0; j < 10; j++) {
         if(notas[j] > notas[j+1]) {
             aux = notas[j];
             notas[j] = notas[j+1];
             notas[j+1] = aux;
         }
     }
-    printf("o maior numero digitado foi: %2.f \n\n", notas[2]);
+    printf("o maior numero digitado foi: %2.f \n\n", notas[9]);
     system("pause");
 }
