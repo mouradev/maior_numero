422def07fe3ce3dedde406c3a9e60a3b9af08821
Lucas Moura <lucasmoura765@gmail.com>
2015-10-15 13:11:16 -0300

    arquivos prontos

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+    float notas[3], aux = 0;
+    int i, j;
+
+    for(i = 0; i < 3; i++) {
+        printf("digite a %da nota: ", i+1);
+        scanf("%f", &notas[i]);
+    }
+
+    for(j = 0; j < 3; j++) {
+        if(notas[j] > notas[j+1]) {
+            aux = notas[j];
+            notas[j] = notas[j+1];
+            notas[j+1] = aux;
+        }
+    }
+    printf("o maior numero digitado foi: %2.f: \n\n", notas[2]);
+    system("pause");
+}
